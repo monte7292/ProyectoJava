3c4c848c3b5567452a2d77b14397810e6508c810
monte <alejandromonterodeltoro@gmail.com>
2025-05-29 00:38:26 +0200

    Update

--- videojuegos/src/main/java/com/dosideas/videojuegos/controller/ListadoController.java
+++ videojuegos/src/main/java/com/dosideas/videojuegos/controller/ListadoController.java
@@ -1,14 +1,30 @@
 package com.dosideas.videojuegos.controller;
 
+import org.springframework.ui.Model;
+import com.dosideas.videojuegos.domain.Videojuego;
+import com.dosideas.videojuegos.service.VideojuegoService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ListadoController {
+    
+    private final VideojuegoService videojuegoService;
+
+    public ListadoController(VideojuegoService videojuegoService) {
+        this.videojuegoService = videojuegoService;
+    }
+    
+    
+    
     //Esto basicamente indica que el mapping es la ruta que rediccionará
     //Esto returna listado en la funcion videojuego que eso es el HTML
     @RequestMapping("/")
-    public String listaVideoJuegos(){
+    public String listarVideoJuegos(Model model){
+        List<Videojuego> destacados = videojuegoService.buscarDestacados();
+        model.addAttribute("videojuegos", destacados);
+        
         //Busqueda de Videojuegos
         return "listado";
     }
